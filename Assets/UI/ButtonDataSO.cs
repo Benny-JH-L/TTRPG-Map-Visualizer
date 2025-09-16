027bf08db518a892597937a1466fdfa65eaf04a7
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2025-09-16 03:12:05 -0600

    Prefab update & ButtonData
    Added
    - button data
    - come debug statements
    
    changes
    - action template; removed gameevent listener
    - added button data on TTRPG_Button
    - `rightPanel` updated with updated prefabs

---
+++ Assets/UI/ButtonDataSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ButtonDataSO", menuName = "Scriptable Objects/ButtonDataSO")]
+public class ButtonDataSO : ScriptableObject
+{
+    
+}
--- Assets/UI/TTRPG_AddButton.cs
+++ Assets/UI/TTRPG_AddButton.cs
@@ -3,18 +3,30 @@ using UnityEngine;
 
 public class TTRPG_AddButton : AbstractUI
 {
+    private static string _debugStart = "TTRPG_AddButton | ";
+
     /// <summary>
     /// the prefab that is added when this button is pressed.
     /// </summary>
-    public GameObject prefabToAdd;  
+    public GameObject prefabToAdd;
+
+    public ButtonDataSO data;
+
+    public GameEvent clickedEvent;
 
     public override void Configure()
     {
-        throw new System.NotImplementedException();
+        Debug.Log($"{_debugStart}Configure not implmented");
     }
 
     public override void Setup()
     {
-        throw new System.NotImplementedException();
+        Debug.Log($"{_debugStart}Setup not implmented");
+    }
+
+    public void OnClick()
+    {
+        Debug.Log("Action add button clicked");
+        clickedEvent.Raise(this, prefabToAdd);
     }
 }
