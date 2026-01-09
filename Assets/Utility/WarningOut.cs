d07333054dfaa5fc2b8b64739c479806a093330b
Benny-JH-L <133819720+Benny-JH-L@users.noreply.github.com>
2026-01-09 16:49:39 -0700

    moved files to `Utility` directory

---
+++ Assets/Utility/WarningOut.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class WarningOut
+{
+
+    /// <summary>
+    /// Calls the Unity `Debug.LogWarning(...)` and structures the debug message.
+    /// </summary>
+    /// <param name="caller"></param>
+    /// <param name="message"></param>
+    public static void Log(object caller, string message)
+    {
+        Debug.LogWarning($"[{caller.GetType()}] {message}");
+    }
+
+}
+
